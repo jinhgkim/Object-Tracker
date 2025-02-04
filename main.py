20e8919cf1104fa9d81128b3d99f41d095d4019c
jinniekim <jinhgkim@gmail.com>
2025-02-04 00:19:53 -0800

    detect objects

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-"""Object Detector App Entry Point"""
+"""Object Tracker App Entry Point"""
 
 import sys
+import cv2 as cv
+import numpy as np
+from ultralytics import YOLO
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -32,8 +35,37 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
 
     def start_detection(self):
-        """Object detection starting point and switch to camera window"""
+        """Starts object detection in the new camera window"""
         print("Starting detection")
+        cap = cv.VideoCapture(0)
+        model = YOLO("yolo11n.pt")
+
+        if not cap.isOpened():
+            print("Cannot open camera")
+            exit()
+
+        while True:
+            # Read a frame
+            success, frame = cap.read()
+
+            if not success:
+                print("Can't receive frame (stream end?). Exiting ...")
+                break
+
+            # Run YOLO11 tracking on the frame, persisting tracks between frames
+            results = model.track(frame, persist=True)
+
+            # Visualize the results on the frame
+            annotated_frame = results[0].plot()
+
+            # Display the annotated frame
+            cv.imshow("YOLO11 Tracking", annotated_frame)
+            if cv.waitKey(1) == ord("q"):
+                break
+
+        # When everything done, release the capture
+        cap.release()
+        cv.destroyAllWindows()
 
 
 if __name__ == "__main__":
